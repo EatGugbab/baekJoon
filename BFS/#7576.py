9d82b94916dad9f6fda1b7790474dbd39b525d6f
EatGugbab <qaswq0530@hs.ac.kr>
2021-06-20 17:57:29 +0900

    #7576

--- BFS/#7576.py
+++ BFS/#7576.py
@@ -29,4 +29,55 @@
 
 # example input
 #
+# 6 4
+# 0 0 0 0 0 0
+# 0 0 0 0 0 0
+# 0 0 0 0 0 0
+# 0 0 0 0 0 1
 #
+# 2 2
+# 1 -1
+# -1 1
+
+from collections import deque
+
+m, n = map(int, input().split())
+graph = []
+data = deque()
+for i in range(n):
+    row = list(map(int, input().split()))
+    for j in range(len(row)):
+        if row[j] == 1:
+            data.append([i, j])
+    graph.append(row)
+
+
+# BFS 탐색
+def bfs(n, m, data):
+    # 상,하,좌,우
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+
+    ans = -1
+    while data:
+        ans += 1
+        for _ in range(len(data)):
+            x, y = data.popleft()
+            # 4방향 탐색
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                # 칸안일 경우
+                if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 0:
+                    graph[nx][ny] = 1
+                    data.append([nx, ny])
+
+    # 익어야 할 토마토가 있는지 체크
+    for i in graph:
+        if 0 in i:
+            return -1
+
+    return ans
+
+
+print(bfs(n, m, data))
